fa9266a8898ea6304868d76815ec2fc5bab62fa2
Ilya Sukharev <imsworkfor@gmail.com>
2023-03-19 18:29:41 +0300

    add

--- src/componnents/App.jsx
+++ src/componnents/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import styles from '../styles.css'
 // Хуки находятся в react-redux
+import { Button } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 // Импортируем нужные действия
 import { decrement, increment, incrementByAmount } from '../slices/counterSlices';
@@ -11,24 +13,34 @@ const App = () => {
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <div>
-        <button
+<div className="container pt-5">
+<h1 className="heading">
+    <span>Redux</span>
+    <button className="btn btn-dark" id="theme">Сменить тему</button>
+  </h1>
+
+  <hr/>
+    
+    <div className="card">
+    <div className="card-body">
+    <h5 className="card-title">Счетчик:{count} <span id="counter"></span></h5>
+        <button type="button"
+          className="btn btn-success"
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
         >
           Прибавить
         </button>
-        <span>{count}</span>
-        <button
+        <button type="button"
+          className="btn btn-danger"
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
           Отнять
         </button>
-        <br />
-        <button onClick={() => dispatch(incrementByAmount(42))}>Прибавить 42</button>
+        <Button onClick={() => dispatch(incrementByAmount(42))}>Прибавить 42</Button>
       </div>
+    </div>
     </div>
   );
 };
